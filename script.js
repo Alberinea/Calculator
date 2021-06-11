1c37cce9f8adfa84717633c1f7bcbdf2c0e70a35
Alberinea <kan9870@gmail.com>
2021-06-12 01:20:17 +0900

    Added math operations

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const buttons = document.querySelectorAll('.num');
 const current = document.querySelector('.current');
 const display = document.querySelector('.display');
+const addButton = document.querySelector('#add');
+const subtractButton = document.querySelector('#subtract');
+const multiplyButton = document.querySelector('#multiply');
+const divideButton = document.querySelector('#divide');
+const operateButton = document.querySelector('#operate');
+let funcOn = false;
 
 function printNum() {
     let num = this.innerText;
@@ -12,8 +18,50 @@ function printNum() {
     current.innerText += num;
 }
 
-buttons.forEach((button) => button.addEventListener('click', printNum));
-
-function add(num1, num2) {
-    return num1 + num2;
+function add() {
+    if (!funcOn) {
+        funcOn = true;
+        current.innerText += ' +\xa0';
+        display.innerText = '';
+    }
 }
+
+function subtract() {
+    if (!funcOn) {
+        funcOn = true;
+        current.innerText += ' -\xa0';
+        display.innerText = '';
+    }
+}
+
+function multiply() {
+    if (!funcOn) {
+        funcOn = true;
+        current.innerText += ' ×\xa0';
+        display.innerText = '';
+    }
+}
+
+function divide() {
+    if (!funcOn) {
+        funcOn = true;
+        current.innerText += ' ÷\xa0';
+        display.innerText = '';
+    }
+}
+
+function operate() {
+    if (!funcOn) {
+        funcOn = true;
+        let result = eval(current.innerText);
+        current.innerText += ' =';
+        display.innerText = result;
+    }
+}
+
+buttons.forEach((button) => button.addEventListener('click', printNum));
+addButton.addEventListener('click', add);
+subtractButton.addEventListener('click', subtract)
+multiplyButton.addEventListener('click', multiply)
+divideButton.addEventListener('click', divide)
+operateButton.addEventListener('click', operate);
