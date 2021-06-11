8ab108ebdd14494ba070b59d413734e769476681
Alberinea <kan9870@gmail.com>
2021-06-12 00:44:26 +0900

    Added print function

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+const buttons = document.querySelectorAll('.num');
+const current = document.querySelector('.current');
+const display = document.querySelector('.display');
 
+function printNum() {
+    let num = this.innerText;
+    let displayScreen = display.innerText.replace(/^0+/, '');
+    let currentScreen = current.innerText.replace(/^0+/, '');
+    display.innerText = displayScreen;
+    current.innerText = currentScreen;
+    display.innerText += num;
+    current.innerText += num;
+}
+
+buttons.forEach((button) => button.addEventListener('click', printNum));
 
 function add(num1, num2) {
-    return num1 + num2
+    return num1 + num2;
 }
