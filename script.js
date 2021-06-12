c1a40ffe113d5957de39593c229601abd5591aad
Alberinea <kan9870@gmail.com>
2021-06-13 08:55:02 +0900

    Added keyboard event listeners

--- script.js
+++ script.js
@@ -24,9 +24,7 @@ function printNum() {
     display.innerText = displayScreen;
     currentLog.innerText = currentScreen;
     display.innerText += num;
-    if (!funcInit) {
-        currentLog.innerText += num;
-    }
+    if (!funcInit) currentLog.innerText += num;
     if (funcUsing) {
         display.innerText = num;
         funcUsing = false;
@@ -51,7 +49,8 @@ function operate() {
         if (dotExisted) {
             dotUsed = false;
         }
-        let storage = currentLog.innerText.replace('×', '*').replace('÷', '/').replace('Mod', '%') + '\xa0' + display.innerText;
+        let storage =
+            currentLog.innerText.replace('×', '*').replace('÷', '/').replace('Mod', '%') + '\xa0' + display.innerText;
         let result = eval(storage);
         currentLog.innerText += '\xa0' + display.innerText + ' =';
         display.innerText = result;
@@ -59,9 +58,7 @@ function operate() {
 }
 
 function add() {
-    if (currentLog.innerText.includes('+')) {
-        operate();
-    }
+    if (currentLog.innerText.includes('+')) operate();
     if (!funcInit) {
         operatorFinished = false;
         funcInit = true;
@@ -76,9 +73,7 @@ function add() {
 }
 
 function subtract() {
-    if (currentLog.innerText.includes('-')) {
-        operate();
-    }
+    if (currentLog.innerText.includes('-')) operate();
     if (!funcInit) {
         operatorFinished = false;
         funcInit = true;
@@ -93,9 +88,7 @@ function subtract() {
 }
 
 function multiply() {
-    if (currentLog.innerText.includes('×')) {
-        operate();
-    }
+    if (currentLog.innerText.includes('×')) operate();
     if (!funcInit) {
         operatorFinished = false;
         funcInit = true;
@@ -110,9 +103,7 @@ function multiply() {
 }
 
 function divide() {
-    if (currentLog.innerText.includes('÷')) {
-        operate();
-    }
+    if (currentLog.innerText.includes('÷')) operate();
     if (!funcInit) {
         operatorFinished = false;
         funcInit = true;
@@ -127,9 +118,7 @@ function divide() {
 }
 
 function reminder() {
-    if (currentLog.innerText.includes('Mod')) {
-        operate();
-    }
+    if (currentLog.innerText.includes('Mod')) operate();
     if (!funcInit) {
         operatorFinished = false;
         funcInit = true;
@@ -166,9 +155,7 @@ function del() {
         if (parseInt(display.innerText) < 10 && !display.innerText.includes('.')) {
             display.innerText = '00';
         }
-        if (!dotExisted) {
-            dotUsed = false;
-        }
+        if (!dotExisted) dotUsed = false;
         display.innerText = display.innerText.slice(0, -1);
     } else if (operatorFinished) {
         currentLog.innerText = '';
@@ -176,15 +163,30 @@ function del() {
 }
 
 function dot() {
-    if (operatorFinished) {
-        dotUsed = true;
-    }
-    if (dotExisted) {
-        dotUsed = true;
-    } else if (!dotUsed && !funcUsing) {
+    if (operatorFinished) dotUsed = true;
+    if (dotExisted) dotUsed = true;
+    else if (!dotUsed && !funcUsing) {
         display.innerText += '.';
         dotUsed = true;
     }
+}
+
+function addKeyboard(e) {
+    const keyboard = document.querySelector(`button[data-key="${e.key}"]`);
+    if (!keyboard) return;
+    if (parseInt(keyboard.innerText) < 10) {
+        this.innerText = keyboard.innerText;
+        printNum();
+    }
+    if (keyboard.innerText === '+') add();
+    if (keyboard.innerText === '−') subtract();
+    if (keyboard.innerText === '×') multiply();
+    if (keyboard.innerText === '÷') divide();
+    if (keyboard.innerText === '+/-') convertAbs();
+    if (keyboard.innerText === '=') operate();
+    if (keyboard.innerText === 'C') clear();
+    if (keyboard.innerText === 'DEL') del();
+    if (keyboard.innerText === '.') dot();
 }
 
 buttons.forEach((button) => button.addEventListener('click', printNum));
@@ -198,5 +200,5 @@ operateButton.addEventListener('click', operate);
 clearButton.addEventListener('click', clear);
 deleteButton.addEventListener('click', del);
 dotButton.addEventListener('click', dot);
+window.addEventListener('keydown', addKeyboard);
 
-console.log(eval('5 -- 5'));
